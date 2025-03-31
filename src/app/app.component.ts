4a88471227704fae21f6d6336f26113c9eebac11
nikitimi <giancarloacarranza@gmail.com>
2025-03-31 08:06:36 +0800

    chore: remove default block, and added variables to be referenced in scss.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '@/components/header/header.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'distort';
+  description = 'A web application clone of Discord.';
 }
